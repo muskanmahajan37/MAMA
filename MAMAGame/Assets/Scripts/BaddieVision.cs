426e5308745092eef114a95b1fd0abf0ff03e186
Arthur Bacon <bacon247@gmail.com>
2018-02-13 10:30:39 -0500

    Added view lines

--- MAMAGame/Assets/Scripts/BaddieVision.cs
+++ MAMAGame/Assets/Scripts/BaddieVision.cs
@@ -8,21 +8,51 @@ public class BaddieVision : MonoBehaviour {
     public float flipTime;
 
     float playerEnterTime;
+    private bool spottedPlayer;
+    private Color origionalColor;
     private float lastFlipTime;
+    private LineRenderer lr;
 
 	// Use this for initialization
 	void Start () {
         playerEnterTime = 0.0f;
         this.lastFlipTime = Time.time;
+        spottedPlayer = false;
+        this.origionalColor = this.GetComponent<SpriteRenderer>().color;
+
+        this.lr = GetComponent<LineRenderer>();
+        //lr.SetPosition(0, this.transform.position);
+        lr.positionCount = this.GetComponent<PolygonCollider2D>().points.Length;
+        for (int i  = 0; i < lr.positionCount; i++)
+        {
+            Vector3 temp = this.GetComponent<PolygonCollider2D>().points[i];
+            lr.SetPosition(i, temp);
+        }
+        
+        lr.SetPosition(0, Vector3.zero);
+        lr.SetPosition(1, this.GetComponent<PolygonCollider2D>().points[6]);  // TODO: Change this 7. I know from experiment 6 works in this case
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (playerEnterTime != 0.0f && Time.time - playerEnterTime > alertTime)
+        if (spottedPlayer)
         {
-            print("GAME OVER");
-            playerEnterTime = Time.time;
+            Color temp = this.GetComponent<SpriteRenderer>().color;
+            float red = temp.r + ((Time.time - playerEnterTime) / alertTime) * 0.02f;
+            Vector3 colors = new Vector3(red, temp.g, temp.b);
+            colors.Normalize();
+            temp.r = colors.x;
+            temp.g = colors.y;
+            temp.b = colors.z;
+            this.GetComponent<SpriteRenderer>().color = temp;
+            if (Time.time - playerEnterTime > alertTime)
+            {
+                print("GAME OVER");
+                playerEnterTime = Time.time;
+            }
         }
+        
         
         if (Time.time - lastFlipTime > flipTime)
         {
@@ -39,6 +69,7 @@ public class BaddieVision : MonoBehaviour {
         if (collision.gameObject.CompareTag("Player"))
         {
             // If the player has entered our 'vision' triger
+            spottedPlayer = true;
             playerEnterTime = Time.time;
         }
 
@@ -49,7 +80,9 @@ public class BaddieVision : MonoBehaviour {
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            spottedPlayer = false;
             playerEnterTime = 0.0f;
+            this.GetComponent<SpriteRenderer>().color = this.origionalColor;
         }
     }
 }
