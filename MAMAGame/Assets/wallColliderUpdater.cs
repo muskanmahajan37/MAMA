e1981c6eabff1160179e347b97daf2e3e5d8d46d
Arthur Bacon <bacon247@gmail.com>
2018-04-02 20:58:29 -0400

    Updated level 1

---
+++ MAMAGame/Assets/wallColliderUpdater.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallColliderUpdater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        BoxCollider2D coll = this.GetComponent<BoxCollider2D>();
+        SpriteRenderer sr = this.GetComponent<SpriteRenderer>();
+
+        coll.size = sr.size;
+	}
+	
+}
