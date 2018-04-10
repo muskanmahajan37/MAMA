d9a7703d07fabe7e9f31eb68f6fab6efd50fd3cd
Arthur Bacon <bacon247@gmail.com>
2018-04-10 14:42:17 -0400

    Added a timer, fixed background... kinda

--- MAMAGame/Assets/Scripts/LevelController.cs
+++ MAMAGame/Assets/Scripts/LevelController.cs
@@ -6,41 +6,56 @@ using UnityEngine.UI;
 
 public class LevelController : MonoBehaviour {
 
-    public GameObject grody;
-    public GameObject tubular;
+  public GameObject grody;
+  public GameObject tubular;
+  private float startTime;
+  private Text timer;
+  private bool levelOver;
 
 	// Use this for initialization
 	void Start () {
-		
+    this.levelOver = false;
+    this.startTime = Time.time;
+    this.timer = GameObject.Find("Canvas/Timer").GetComponent<Text>();
 	}
 
-    public void levelWon()
-    {
-        this.tubular.SetActive(true);
-    }
-	
+
+  void Update() {
+      if (!this.levelOver) {
+          this.timer.text = (Time.time - startTime).ToString("0.00");
+          print(this.levelOver);
+      }
+  }
+
+  public void levelWon()
+  {
+      this.levelOver = true;
+      this.tubular.SetActive(true);
+  }
+
 	public void gameOver() {
-		print("Game over in LevelController");
-        var awayPos = new Vector3(-1000, -1000, -1000);
-        GameObject.Find("Player").transform.position = awayPos;
-        this.grody.SetActive(true);
+      this.levelOver = true;
+      print(this.levelOver);
+      var awayPos = new Vector3(-1000, -1000, -1000);
+      GameObject.Find("Player").transform.position = awayPos;
+      this.grody.SetActive(true);
 	}
 
-    public void restartLevel()
-    {
-        print("restartLevel GameController");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
+  public void restartLevel()
+  {
+      print("restartLevel GameController");
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+  }
 
-    public void quit()
-    {
-        print("quit GameController");
-        Application.Quit();
-    }
+  public void quit()
+  {
+      print("quit GameController");
+      Application.Quit();
+  }
 
-    public void nextLevel()
-    {
-        print("nextLevel GameController");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+  public void nextLevel()
+  {
+      print("nextLevel GameController");
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
 }
