acaf4541beca09545ca6c7f291042a22e0dc6c46
Arthur Joseph Bacon <bacon.a@northeastern.edu>
2018-04-10 19:16:29 -0400

    Added crystal blockers

---
+++ MAMAGame/Assets/CrystalAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalAnimator : MonoBehaviour {
+
+    public List<Sprite> crystalSprites;
+
+    private int currentIndex;
+    private float lastChange;
+    private float timeBetweenFrameChange = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        this.lastChange = Time.time;
+        this.currentIndex = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - lastChange > this.timeBetweenFrameChange)
+        {
+            this.lastChange = Time.time;
+            this.currentIndex = (currentIndex + 1) % this.crystalSprites.Count;
+            this.GetComponent<SpriteRenderer>().sprite = crystalSprites[currentIndex];
+        }
+	}
+}
